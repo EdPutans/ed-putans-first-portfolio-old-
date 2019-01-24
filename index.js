01e3b401e798be30b0c98dbf993a6e588170f765
Edgar <edputans@gmail.com>
2019-01-24 23:21:01 +0000

    commit of whatever is done to try and push to netlify

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-let e = 0
+let navList = document.querySelector('.topbar-list')
+let menuButton = document.querySelector('.burger-button')
+
+let navBarOpen = false
+
+menuButton.addEventListener('click', e =>{
+    e.preventDefault()
+    navBarOpen = !navBarOpen
+    navList.hidden = navBarOpen
+})
