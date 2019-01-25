c1328c5ff34e95ba79ed85afebbcfe913403378e
EdPutans <edputans@gmail.com>
2019-01-25 11:49:27 +0000

    added unfolding stuff to cards

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
-// let navList = document.querySelector('.topbar-list')
-// let menuButton = document.querySelector('.burger-button')
-// navList.hidden = true
 
-// menuButton.addEventListener('click', e =>{
-//     e.preventDefault()
-//     navList.hidden = !navList.hidden
-// })
+const cardHeights = []
+const projectCards = document.querySelectorAll('.project-card')
+let index = 0
+projectCards.forEach(card=>{
+    card.addEventListener('click', ()=>{
+        let p = card.querySelectorAll('p')
+        let h4 = card.querySelector('h4')
+        h4.style.display = h4.style.display !== ''? '' : 'block'
+        p.forEach(p=>p.style.display = p.style.display !== ''? '' : 'block')
+    })
+})
