7a54fda7a8898c83e04a90fa71408bd231952430
EdPutans <edputans@gmail.com>
2019-01-29 21:34:59 +0000

    finishing changes for tuesday

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 
-const cardHeights = []
-const projectCards = document.querySelectorAll('.project-card')
-let index = 0
-projectCards.forEach(card=>{
-    card.addEventListener('click', ()=>{
-        let p = card.querySelectorAll('p')
-        let h4 = card.querySelector('h4')
-        h4.style.display = h4.style.display !== ''? '' : 'block'
-        p.forEach(p=>p.style.display = p.style.display !== ''? '' : 'block')
-    })
-})
+// const cardHeights = []
+// const projectCards = document.querySelectorAll('.project-card')
+// let index = 0
+// projectCards.forEach(card=>{
+//     card.addEventListener('click', ()=>{
+//         let p = card.querySelectorAll('p')
+//         let h4 = card.querySelector('h4')
+//         h4.style.display = h4.style.display !== ''? '' : 'block'
+//         p.forEach(p=>p.style.display = p.style.display !== ''? '' : 'block')
+//     })
+// })
